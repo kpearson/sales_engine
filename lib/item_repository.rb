6d733b221800426a46d995cb2c1f9441f979e52d
Kit <dev_kit@icloud.com>
2014-11-26 20:47:18 -0700

    add items repo

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -16,4 +16,25 @@ class ItemRepository
     end
   end
 
+  def all
+    items
+  end
+
+  def find_by_id(id)
+    items.find do |item|
+      item.id == id
+    end
+  end
+
+  def find_by_name(name)
+    items.find do |item|
+      item.name == name
+    end
+  end
+
+  def find_by_description(description)
+    items.find do |item|
+      item.description == description
+    end
+  end
 end
