6fe6e5ce4ef2b355d885b4fead4177a8a524c8ac
Andrew Overby <andrewdoverby@gmail.com>
2014-11-28 14:50:28 -0700

    merchant and transaction files created

---
+++ lib/transaction.rb
@@ -0,0 +1,17 @@
+class Transaction
+  attr_reader :id,
+              :invoice_id,
+              :credit_card_number,
+              :credit_card_expiration_date,
+              :result,
+              :created_at,
+              :updated_at
+
+  def initialize(data)
+    @id         = data[:id]
+    @first_name = data[:first_name]
+    @last_name  = data[:last_name]
+    @created_at = data[:created_at]
+    @updated_at = data[:updated_at]
+  end
+end
